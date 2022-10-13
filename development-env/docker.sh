f803ff683a40c69de7d77057f4c30dbc151767c2
arcosx <arcosx@outlook.com>
2022-10-13 15:21:24 +0800

    Update docker.sh

--- development-env/docker.sh
+++ development-env/docker.sh
@@ -1,2 +1,17 @@
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
+
+# [option] set proxy for pull image
+source ~/.proxyenv.sh
+mkdir /etc/systemd/system/docker.service.d/
+cat <<EOT > /etc/systemd/system/docker.service.d/http-proxy.conf
+[Service]
+Environment="HTTP_PROXY=$HTTP_PROXY"
+Environment="HTTPS_PROXY=$HTTPS_PROXY"
+Environment="NO_PROXY=$NO_PROXY"
+EOT
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo systemctl show --property=Environment docker
+docker info | grep Proxy
+
